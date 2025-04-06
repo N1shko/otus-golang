c30d4b5eef8a14305ee30d14511d6a2cb4158949
flages1325 <flages1325@gmail.com>
2025-04-06 18:13:40 +0300

    added initial solution

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	str = strings.Replace(str, "\n", " ", -1)
+	str = strings.Replace(str, "\t", " ", -1)
+	split := strings.Split(str, " ")
+	wordsMapped := map[string]int{}
+	for _, word := range split {
+		if word == "" {
+			continue
+		}
+		if val, ok := wordsMapped[word]; ok {
+			wordsMapped[word] = val + 1
+		} else {
+			wordsMapped[word] = 1
+		}
+	}
+
+	sliceSorted := make([]string, 0, len(wordsMapped))
+	for key := range wordsMapped {
+		sliceSorted = append(sliceSorted, key)
+	}
+	sort.Slice(sliceSorted, func(i, j int) bool {
+		if wordsMapped[sliceSorted[i]] == wordsMapped[sliceSorted[j]] {
+			return sliceSorted[i] < sliceSorted[j]
+		} else {
+			return wordsMapped[sliceSorted[i]] > wordsMapped[sliceSorted[j]]
+		}
+	})
+	if len(sliceSorted) < 10 {
+		return sliceSorted
+
+	} else {
+		return sliceSorted[:10]
+	}
 }
