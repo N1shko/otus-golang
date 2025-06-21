3c9efed079a1c2a63fc50a4d588059f1ae8867f4
flages1325 <flages1325@gmail.com>
2025-06-21 16:51:06 +0300

    additional unit-tests and refactor to for-select with done channel

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,32 +10,53 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	var errCount int64
 	var wg sync.WaitGroup
+	var once sync.Once
 
 	taskPool := make(chan Task)
-	defer func() {
-		close(taskPool)
-		wg.Wait()
-	}()
+	done := make(chan struct{})
+
 	wg.Add(n)
 	for i := 0; i < n; i++ {
 		go func() {
-			for taskFromPool := range taskPool {
-				if err := taskFromPool(); err != nil {
-					atomic.AddInt64(&errCount, 1)
+			defer wg.Done()
+			for {
+				select {
+				case <-done:
+					return
+				case task, ok := <-taskPool:
+					if !ok {
+						return
+					}
+					if err := task(); err != nil {
+						if atomic.AddInt64(&errCount, 1) >= int64(m) {
+							once.Do(func() {
+								close(done)
+							})
+						}
+					}
 				}
 			}
-			wg.Done()
 		}()
 	}
+
 	for _, task := range tasks {
-		if atomic.LoadInt64(&errCount) >= int64(m) {
+		select {
+		case <-done:
+			close(taskPool)
+			wg.Wait()
 			return ErrErrorsLimitExceeded
+		case taskPool <- task:
 		}
-		taskPool <- task
+	}
+
+	close(taskPool)
+	wg.Wait()
+
+	if atomic.LoadInt64(&errCount) >= int64(m) {
+		return ErrErrorsLimitExceeded
 	}
 	return nil
 }
