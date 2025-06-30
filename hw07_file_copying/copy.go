7debea20291794c6663ce4433aa900dd5d3532a2
N1shko <72214293+N1shko@users.noreply.github.com>
2025-06-30 17:13:07 +0300

    initial solution for hw 7 (#9)
    Co-authored-by: flages1325 <flages1325@gmail.com>

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint
 )
 
 var (
@@ -10,6 +15,47 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, 0o444)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	defer src.Close()
+	oldFileStat, err := src.Stat()
+	if err != nil {
+		fmt.Print(err)
+		return ErrUnsupportedFile
+	}
+	// check if offset is correct and return immediately if it is not
+	if oldFileStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	defer newFile.Close()
+	if offset > 0 {
+		_, err = src.Seek(offset, io.SeekStart)
+		if err != nil {
+			fmt.Print(err)
+			return err
+		}
+	}
+	oldReader := io.Reader(src)
+	if offset+limit > oldFileStat.Size() {
+		// for correct barReader work
+		limit = oldFileStat.Size() - offset
+	}
+	if limit > 0 {
+		oldReader = io.LimitReader(src, limit)
+	}
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(oldReader)
+	if _, err := io.Copy(newFile, barReader); err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("copy failed: %w", err)
+	}
+	bar.Finish()
 	return nil
 }
