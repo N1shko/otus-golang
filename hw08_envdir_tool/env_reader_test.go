1f6171567b0ac57f1ccfd9d98b6f1be74360cd8b
N1shko <72214293+N1shko@users.noreply.github.com>
2025-07-01 22:07:04 +0300

    add initial solution for hw08 (#10)
    Co-authored-by: flages1325 <flages1325@gmail.com>

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("shell script test", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+		require.Nil(t, err)
+		require.Equal(t, EnvValue{"\"hello\"", false}, env["HELLO"])
+		require.Equal(t, EnvValue{"bar", false}, env["BAR"])
+		require.Equal(t, EnvValue{"   foo\nwith new line", false}, env["FOO"])
+		require.Equal(t, EnvValue{"", true}, env["UNSET"])
+		require.Equal(t, EnvValue{"", false}, env["EMPTY"])
+	})
+
+	t.Run("empty dir", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		env, err := ReadDir(tmpDir)
+		require.Nil(t, err)
+		require.Equal(t, Environment{}, env)
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,47 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("simple test", func(t *testing.T) {
+		testEnv := Environment{
+			"BAR": EnvValue{"bar", false},
+		}
+		os.Setenv("ADDED", "original")
+		cmd := []string{"sh", "-c", "echo -n $BAR $ADDED"}
+
+		// forwarding stdout for capturing
+		oldStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		// executing command
+		exitCode := RunCmd(cmd, testEnv)
+
+		w.Close()
+		var buf bytes.Buffer
+		_, _ = io.Copy(&buf, r)
+
+		// taking stdout back to normal
+		os.Stdout = oldStdout
+		capturedOutput := buf.String()
+		require.Equal(t, "bar original", capturedOutput)
+		require.Equal(t, 0, exitCode)
+	})
+
+	t.Run("exit code simple test", func(t *testing.T) {
+		testEnv := Environment{
+			"BAR": EnvValue{"bar", false},
+		}
+		cmd := []string{"sh", "-c", "exit 123"}
+		exitCode := RunCmd(cmd, testEnv)
+		require.Equal(t, 123, exitCode)
+	})
 }
